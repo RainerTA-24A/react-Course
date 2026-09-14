04a4dd7b995bf0757647ce4949c728f53d53ba25
Rainer T <rainer.tahker@tptlive.ee>
2026-09-14 22:14:04 +0300

    Basic Routing Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignupForm from "./components/SignUpForm";
 //import { useState } from "react";
 
+function HomePage() {
+  return <h1>Home Page</h1>
+}
+
+function AboutPage() {
+  return <h1>About Page</h1>
+}
+
 function App() {
 
-  return <div><SignupForm /> </div>;
-
+  return (
+    <div>
+      <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem"}}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes> 
+      <div>Footer</div>
+    </div>
+  );
 }
 
 export default App;
