d89f695e90ab3d464ece2bca8313ecc58736323a
Rainer T <rainer.tahker@tptlive.ee>
2026-09-14 21:42:39 +0300

    Better form

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,33 +1,64 @@
-//import { useState } from "react";
-
+import { useForm } from 'react-hook-form'
 import { useState } from "react";
 
 export default function SignupForm() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const {register, handleSubmit, formState: {errors} } = useForm();
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    alert(`Submitted with email:  ${email} and password: ${password}`);
+  function onSubmit(data) {
+    alert(`Submitted with email:  ${data.email} and password: ${data.password}`);
   }
 
   return (
     <div style={{ maxWidth: 400, margin: "2rem auto" }}>
       <h1>Sign Up</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div style={{ marginBottom: "1rem" }}>
           <label>
             Email
-            <input type="email" placeholder="you@example.com" onChange={(e) => setEmail(e.target.value)}/>
+            <input
+              type="email"
+              placeholder="you@example.com"
+              {...register("email", {
+                required: "Email is required",
+                minLength: {
+                    value: 7,
+                    message: "Email must be atleast 7 chars",
+                },
+                maxLength: {
+                    value: 25,
+                    message: "Email can not be longer than 25 letters",
+                },
+                })} />
           </label>
+
+          {errors.email && (
+            <p style={{ color: "crimson"}}>{errors.email.message}</p>
+          )}
         </div>
 
         <div style={{ marginBottom: "1rem" }}>
           <label>
             Password
-            <input type="password" placeholder="••••••••" onChange={(e) => setPassword(e.target.value)}/>
+            <input
+              type="password"
+              placeholder="••••••••"
+              {...register("password", {
+                required: "Password is required", 
+                minLength: {
+                    value: 4,
+                    message: "Password must be atleast 4 chars",
+                },
+                maxLength: {
+                    value: 25,
+                    message: "Password cant be longer than 25 letters",
+                },
+            })}
+            />
           </label>
+          {errors.password && (
+            <p style={{color: 'violets'}}>{errors.password.message}</p>
+          )}
         </div>
 
         <button type="submit">Create account</button>
