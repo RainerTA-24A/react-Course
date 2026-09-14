b53d1c7dd9e98820fe80d3b60947b3b2771ae845
Rainer T <rainer.tahker@tptlive.ee>
2026-09-14 18:22:31 +0300

    Input UseState Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-import './App.css'
-
+import "./App.css";
 import { useState } from "react";
 
-//Component = function that returns JSX 
-//props or {age}
-//Rerender the component
-function Greeting({age}) {
-  return <h1>Hello! I hate this already, {getName()} {age}</h1>
-}
-
 function App() {
-  const [showGreeting, setShowGreeting] = useState(false);
-  function toggleGreeting() {
-    setShowGreeting(!showGreeting);
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
   }
+
   return (
     <div>
-      <button onClick={toggleGreeting}>Click Me</button>
-       
-      {showGreeting && <Greeting age={"Nigga"} />}
+      <input
+        type="text"
+        placeholder="Name..."
+        value={name}
+        onChange={handleChange}
+      />
+
+      <p>{name}</p>
     </div>
-          
   );
 }
 
-function getName() {
-  return "Rainer";
-}
-
-export default App
+export default App;
